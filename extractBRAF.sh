717db2a8f39e5f2ba8f02449af6e0f859cfe1b7f
doerlbh <doerlbh@gmail.com>
2018-05-23 15:53:05 -0400

    update code

--- extractBRAF.sh
+++ extractBRAF.sh
@@ -9,6 +9,8 @@
 grep -o 's3.*bam' case_s3_bam_loc.tumor.ref.txt > s3_tumor.txt
 grep -o 's3.*bam' case_s3_bam_loc.normal.ref.txt > s3_normal.txt
 
+ssh -i /Users/DoerLBH/Dropbox/git/doerlbh-rabadan.pem ubuntu@ec2-54-152-113-174.compute-1.amazonaws.com
+
 # in AWS instance
 
 mkdir tumor
@@ -17,17 +19,19 @@ mkdir normal
 cd tumor
 for f in `cat s3_tumor.txt`
 do
-	aws s3 cp $f . --dryrun
-    samtools view *.bam "Chr7:140719327-140924764" >> tumor_BRAF.txt
-    rm *.bam
+	aws s3 cp $f . 
+	aws s3 cp $f.bai . 
+    samtools view *.bam "chr7:140719327-140924764" >> tumor_BRAF.txt
+    rm *.bam*
 done
 cd ..
 
 cd normal
 for f in `cat s3_tumor.txt`
 do
-	aws s3 cp $f . --dryrun
-    samtools view *.bam "Chr7:140719327-140924764" >> normal_BRAF.txt
-    rm *.bam
+	aws s3 cp $f .
+	aws s3 cp $f.bai . 
+    samtools view *.bam "chr7:140719327-140924764" >> normal_BRAF.txt
+    rm *.bam*
 done
 cd ..
