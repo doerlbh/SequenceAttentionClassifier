7bdf62deb405b4b7a0eca9d41d502500fe013118
doerlbh <doerlbh@gmail.com>
2018-05-23 14:52:52 -0400

    update code

--- extractBRAF.sh
+++ extractBRAF.sh
@@ -6,4 +6,28 @@
 # Columbia University
 # May 2018
 
+grep -o 's3.*bam' case_s3_bam_loc.tumor.ref.txt > s3_tumor.txt
+grep -o 's3.*bam' case_s3_bam_loc.normal.ref.txt > s3_normal.txt
 
+# in AWS instance
+
+mkdir tumor
+mkdir normal
+
+cd tumor
+for f in `cat s3_tumor.txt`
+do
+	aws s3 cp $f . --dryrun
+    samtools view *.bam "Chr7:140719327-140924764" >> tumor_BRAF.txt
+    rm *.bam
+done
+cd ..
+
+cd normal
+for f in `cat s3_tumor.txt`
+do
+	aws s3 cp $f . --dryrun
+    samtools view *.bam "Chr7:140719327-140924764" >> normal_BRAF.txt
+    rm *.bam
+done
+cd ..
