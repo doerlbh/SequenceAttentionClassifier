9322df00d67e827c57a18193a9fe4b62a252d503
doerlbh <doerlbh@gmail.com>
2018-05-24 16:08:50 -0400

    update code

--- extractBRAF.sh
+++ extractBRAF.sh
@@ -17,6 +17,7 @@ mkdir tumor
 mkdir normal
 
 cd tumor
+
 for f in `cat s3_tumor.txt`
 do
 	exists=$(aws s3 ls $f)
@@ -28,12 +29,31 @@ do
 
 	aws s3 cp $f . 
 	aws s3 cp $f.bai . 
-    samtools view *.bam "chr7:140719327-140924764" | cut -f 10 >> tumor-BRAF.txt
+    samtools view *.bam "chr7:140,719,327-140,924,764" | cut -f 10 >> tumor-BRAF.txt
+    samtools view *.bam "chr7:140,719,327-140,924,764" >> tumor-BRAF-full.txt
     rm *.bam*
 done
+
+for f in `cat s3_tumor_val.txt`
+do
+	exists=$(aws s3 ls $f)
+	if [ -z "$exists" ]; then
+		echo $f >> no_exist.txt
+	else
+		echo $f >> exist.txt
+	fi
+
+	aws s3 cp $f . 
+	aws s3 cp $f.bai . 
+    samtools view *.bam "chr7:140,719,327-140,924,764" | cut -f 10 >> tumor-BRAF-val.txt
+    samtools view *.bam "chr7:140,719,327-140,924,764" >> tumor-BRAF-val-full.txt
+    rm *.bam*
+done
+
 cd ..
 
 cd normal
+
 for f in `cat s3_normal.txt`
 do
 	exists=$(aws s3 ls $f)
@@ -45,7 +65,25 @@ do
 
 	aws s3 cp $f .
 	aws s3 cp $f.bai . 
-    samtools view *.bam "chr7:140719327-140924764" | cut -f 10 >> normal-BRAF.txt
+    samtools view *.bam "chr7:140,719,327-140,924,764" | cut -f 10 >> normal-BRAF.txt
+    samtools view *.bam "chr7:140,719,327-140,924,764" >> normal-BRAF-full.txt
     rm *.bam*
 done
+
+for f in `cat s3_normal_val.txt`
+do
+	exists=$(aws s3 ls $f)
+	if [ -z "$exists" ]; then
+		echo $f >> no_exist.txt
+	else
+		echo $f >> exist.txt
+	fi
+
+	aws s3 cp $f .
+	aws s3 cp $f.bai . 
+    samtools view *.bam "chr7:140,719,327-140,924,764" | cut -f 10 >> normal-BRAF-val.txt
+    samtools view *.bam "chr7:140,719,327-140,924,764" >> normal-BRAF-val-full.txt
+    rm *.bam*
+done
+
 cd ..
